1851ea620a20eefb698e66462b37f1384acffa9a
bouquet1 <bukuleta@gmail.com>
2023-07-28 19:11:28 -0700

    return statement is replaced with print method

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    #if length is zero, return empty list "[]\n"
+    #if length is zero, return empty list "[]\n". If length is 1, print '[0]\n'. "\n" adds a newline character at the end to format the output nicely.
     if length == 0:
-        return "[]\n"
+        # return '[]\n'
+        # return 0
+        print("[]")
     elif length == 1:
-        return "[0]\n"
-    # if length is 1, print 0
-    # if length is greater than 1, generate fibonacci sequence up to provided length. while loop to add new elements until it reaches the provided length
+        # return '[0]\n'
+        # return 0
+        print("[0]")
+    # if length is greater than 1, generate fibonacci sequence up to provided length. while loop to add new elements.
     else:
         fibonacci_list = [0, 1]
         while len(fibonacci_list) < length:
             next_number = fibonacci_list[-1] + fibonacci_list[-2]
             # append the next number to fibonacci_list 
             fibonacci_list.append(next_number)
-        return fibonacci_list
-
-
+        # return "[" + ", ".join(map(str, fibonacci_list)) + "]\n"
+        # return fibonacci_list 
+        print(fibonacci_list)
+    
+        
+        
+# print(print_fibonacci(0)) => returns [] in terminal but the numbers are not separated by comma.
